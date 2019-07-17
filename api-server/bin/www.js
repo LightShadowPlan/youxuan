237715fb7db1474379acb71c41bcfd51ad7e0b37
LightShadowPlan <2103875834@qq.com>
2019-07-17 13:45:00 +0800

    修改小bug

--- api-server/bin/www.js
+++ api-server/bin/www.js
@@ -1,21 +1,12 @@
 const app = require('../app');
 const debug = require('debug')('back-end:server');
 const http = require('http');
-const fs = require('fs');
-const https = require('https');
 const webSocket = require('../controllers/webSocket');
 
-const privateKey = fs.readFileSync('./ssl/0_lightshadow.xyz.key', 'utf8');
-
-const certificate = fs.readFileSync('./ssl/1_lightshadow.xyz_bundle.pem', 'utf8');
-
-const credentials = {key: privateKey, cert: certificate};
 
 const httpserver = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
 
 httpserver.listen(3000);
 
-httpsServer.listen(3443);
 
 webSocket()
